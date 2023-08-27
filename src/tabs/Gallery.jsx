32585140acd130bf024eefa38d6b0d7b947ec64a
psvfx <psvfxinvest@gmail.com>
2023-08-27 12:41:01 +0300

    plus

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -8,54 +8,70 @@ export class Gallery extends Component {
     query: '',
     page: 1,
     images: [],
-  }
+    isEmpty: false,
+    isVisibleButton: false,
+  };
 
-
-  componentDidUpdate(prevProps, prevState){
-   
-    const {query, page} = this.state
-  if(prevState.query !== query){
-    this.getGaleryList(query, page)
-  }
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getGaleryList(query, page);
+    }
   }
 
   getGaleryList = async (query, page) => {
     try {
-      const result = await ImageService.getImages(query, page)
-      console.log(result)
-      this.setState({images: result.photos})
-      
-    } catch (error) {
-      
-    }
-  }
+      const {
+        photos,
+        total_results,
+        per_page,
+        page: currentPage,
+      } = await ImageService.getImages(query, page);
+      this.setState(prevState => ({
+        images: [...prevState.images, ...photos],
+        isVisibleButton: currentPage < Math.ceil(total_results / per_page),
+      }));
+      if (photos.length === 0) {
+        this.setState({ isEmpty: true });
+      }
+    } catch (error) {}
+  };
 
-  onHandleSubmit = (query) =>{
-    this.setState({query});
-  }
+  onHandleSubmit = query => {
+    this.setState({
+      query,
+      page: 1,
+      images: [],
+      isEmpty: false,
+      isVisibleButton: false,
+    });
+  };
 
-
+  onLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
   render() {
-    const { images } = this.state;
+    const { images, isEmpty, isVisibleButton } = this.state;
     return (
       <>
-      <SearchForm onSubmit={this.onHandleSubmit}/>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm onSubmit={this.onHandleSubmit} />
+        {isEmpty && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
         <Grid>
-          {
-            images.map(({ id, alt, src: { small } }) => {
-              return (
-                <GridItem key='id'>
-
-                  <CardItem>
-                    
-    <img src={small} alt={alt} />
-  </CardItem>
-</GridItem>
-              )
-            }) 
-  }
-</Grid>
+          {images.map(({ id, alt, src: { small } }) => {
+            return (
+              <GridItem key={id}>
+                <CardItem>
+                  <img src={small} alt={alt} />
+                </CardItem>
+              </GridItem>
+            );
+          })}
+        </Grid>
+        {isVisibleButton && (
+          <Button onClick={this.onLoadMore}>Load more</Button>
+        )}
       </>
     );
   }
